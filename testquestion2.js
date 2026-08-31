4f01a317b27c093b7642bab532bab34173b60330
Alexpoke2-sketch <balduvia@gmail.com>
2026-08-31 10:44:03 +0000

    Aug 31, 2026, 10:44 AM

--- testquestion2.js
+++ testquestion2.js
@@ -4,7 +4,7 @@ function setup(){
 }
 function draw(){
     for(let count = 0;count< 10; count++ ){
-        fill(count * 30)
+        fill(count * 30);
         circle(30+count*30,30+count*30,30);
     }
 }
