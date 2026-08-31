c4d9df7be78988afaafc27064d09a99adf955653
Alexpoke2-sketch <balduvia@gmail.com>
2026-08-31 10:13:10 +0000

    Aug 31, 2026, 10:13 AM

--- testquestion1.js
+++ testquestion1.js
@@ -12,7 +12,7 @@ function draw(){
     fill("black");
     stroke("red");
     strokeWeight(10);
-    rect(100,200,75,75);
+    rect(0,200,75,75);
     fill("black");
     stroke("red");
     strokeWeight(10);
