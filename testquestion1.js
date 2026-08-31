43ae05c70719108fb374ef8456b5e313fd37b930
Alexpoke2-sketch <balduvia@gmail.com>
2026-08-31 10:11:17 +0000

    Aug 31, 2026, 10:11 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,20 @@
 
+function setup(){
+    createCanvas(600,400);
+    background(0);
+}
+
+function draw(){
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    circle(150,200,80);
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    rect(200,200,50,50);
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    triangle(300,100,350,300,325,250)
+}
